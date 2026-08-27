d5b0370ae5696623c3c0044f97617f680569ac02
present <2641362143@qq.com>
2026-08-27 10:52:02 +0800

    全局异常处理器

--- tlias-web-managemant/src/main/java/tliaswebmanagement/exception/GlobalExceptionHandler.java
+++ tlias-web-managemant/src/main/java/tliaswebmanagement/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tliaswebmanagement.pojo.Result;
 
+
+//
 @Slf4j
 @RestControllerAdvice//统一处理异常
 public class GlobalExceptionHandler {
@@ -13,11 +15,11 @@ public class GlobalExceptionHandler {
     public Result handleException(Exception e){
         log.error("服务器发生异常:{}",e.getMessage());
         e.printStackTrace();
-        return Result.error("服务器异常");
+        return Result.error("服务器异常");//返回给出现异常的controller三层架构
     }
 
     //捕获DuplicateKeyException(数据库数据重复异常)
-    @ExceptionHandler   //当有多个异常处理器可处理抛出来的异常，则从该异常往上找父类异常，直到找到一个匹配的异常处理器
+    @ExceptionHandler   //当有多个异常处理器可处理抛出来的异常，则从该异常往上找父类异常，直到找到一个匹配的异常处理器,即匹配最相关的异常
     public Result handleException(DuplicateKeyException e){
         log.error("服务器发生异常:{}",e.getMessage());
         String message = e.getMessage();//获取异常信息
